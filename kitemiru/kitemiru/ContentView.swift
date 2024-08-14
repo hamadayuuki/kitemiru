5c902d7497c0843a73d7d61121888a5b7c23d0ad
hamadayuuki <yuukirinnma@gmail.com>
2024-08-14 20:26:04 +0900

    Init app project

---
+++ kitemiru/kitemiru/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kitemiru
+//
+//  Created by 濵田　悠樹 on 2024/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kitemiru/kitemiru/kitemiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  kitemiruApp.swift
+//  kitemiru
+//
+//  Created by 濵田　悠樹 on 2024/08/14.
+//
+
+import SwiftUI
+
+@main
+struct kitemiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
